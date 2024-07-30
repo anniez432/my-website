05b56b3191b0c3bfef3c921def8e7aa6ff03c0c7
Annie <zhao15a@outlook.com>
2024-07-30 13:43:52 +0800

    Add js file and images

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const leftButton = document.getElementById("left-button");
+    const rightButton = document.getElementById("right-button");
+    const projectWrapper = document.querySelector(".projects-wrapper");
+    const projectCards = document.querySelectorAll(".project-card")
+
+    let cardIndex = 0;
+
+    function updateCard(){
+        const offset = -cardIndex * 100;
+        projectWrapper.style.transform = `translateX(${offset}%)`;
+    }
+
+    leftButton.addEventListener("click", () => {
+        cardIndex = (cardIndex > 0)? cardIndex - 1 : projectCards.length - 1;
+        updateCard();
+    });
+
+    rightButton.addEventListener("click", () => {
+        cardIndex = (cardIndex < projectCards.length - 1)? cardIndex + 1 : 0;
+        updateCard();
+    });
+
+
+    updateCard();
+})
