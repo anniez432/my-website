a9070545a301f481e5779f2d5695c5dd5616cc7e
Annie <zhao15a@outlook.com>
2024-12-22 16:49:37 -0600

    Added effects to welcome-title

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const leftButton = document.getElementById("left-button");
     const rightButton = document.getElementById("right-button");
     const projectWrapper = document.querySelector(".projects-wrapper");
-    const projectCards = document.querySelectorAll(".project-card")
+    const projectCards = document.querySelectorAll(".project-card");
+
+    const welcomeTitle = document.getElementById("welcome-title");
+    let lastScrollPosition = 0;
 
     let cardIndex = 0;
 
@@ -23,4 +26,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     updateCard();
+
+    window.addEventListener("scroll", () => {
+        const currentScrollPosition = window.scrollY;
+        
+        let scale = 1 - currentScrollPosition / 1000;
+
+        const isScrollingDown = currentScrollPosition > lastScrollPosition;
+
+        if (isScrollingDown) {
+            welcomeTitle.style.transform = `scale(${Math.max(scale, 1.5)}) translate3d(0, -5px, 0)`;
+            
+        } else {
+            // Reverse transformations when scrolling up
+            scale = 1 + currentScrollPosition / 1000; // Grow the title when scrolling up
+            welcomeTitle.style.transform = `scale(${Math.max(scale, 1)}) translate3d(0, 5px, 0)`;
+        }
+
+        welcomeTitle.style.transition = `transform 0.6s ease`;
+    
+        // Update last scroll position
+        lastScrollPosition = currentScrollPosition;
+    })
 })
